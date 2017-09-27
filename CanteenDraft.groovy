82118b7d0e45ea34f051573c44e344d0dd5cacba
Harry Alvin Waidan Kefas <harry.kambey@gmail.com>
2017-09-27 12:07:09 +0700

    add canteen's dsl draft

---
+++ CanteenDraft.groovy
@@ -0,0 +1,165 @@
+package Groovy
+
+import groovy.xml.MarkupBuilder
+
+class TransactionItem {
+    String name
+    int amount
+    int costPerItem
+}
+class Transaction {
+    String type
+    List<TransactionItem> items
+    Date timestamp
+    int totalCost
+}
+
+class StockBuyer {
+    Canteen canteen
+    String ingredientName
+    int ingredientAmount
+    int ingredientCost
+    
+    def buy(name, amount = 1) {
+        ingredientName = name
+        ingredientAmount = amount
+        return this
+    }
+    def at(cost) {
+        ingredientCost = cost
+        return this
+    }
+    def getEach() {
+        canteen.buyStock(ingredientName, ingredientAmount, ingredientCost)
+        ingredientName = null
+    }
+    def getTotal() {
+        ingredientCost = ingredientCost/ingredientAmount
+        canteen.buyStock(ingredientName, ingredientAmount, ingredientCost)
+        ingredientName = null
+    }
+}
+
+class FoodIngredient {
+    String name
+    int amount
+}
+class Food {
+    FoodIngredient[] ingredients
+    int cost
+}
+
+class Canteen {
+    static ingredientStock = [:]
+    static foodMenu = [:]
+    static List<Transaction> transactionLog
+    static int chairs
+    
+    /**
+     * This method accepts a closure which is essentially the DSL. Delegate the 
+     * closure methods to the DSL class so the calls can be processed
+     */
+    def static process(closure) {
+        Canteen c = new Canteen()
+        
+        // If this is the first call, initialize the static variables
+        if(chairs == 0) {
+            chairs = 10
+            ingredientStock."rice" = 0
+            ingredientStock."chicken meat" = 0
+            transactionLog = []
+        }
+        
+        // any method called in closure will be delegated to the memoDsl class
+        closure.delegate = c
+        closure.call()
+    }
+    
+    def stock(closure) {
+        StockBuyer b = new StockBuyer(canteen: this)
+        closure.delegate = b
+        closure.resolveStrategy = Closure.DELEGATE_ONLY
+        
+        def trans = new Transaction(
+            type: "Buying Ingredients",
+            items: [],
+            timestamp: new Date(), 
+            totalCost: 0,
+        )
+        transactionLog << trans
+        
+        closure.call()
+        
+        for(item in transactionLog.last().items) {
+            transactionLog.last().totalCost += item.costPerItem*item.amount
+        }
+        if(transactionLog.last().totalCost == 0) {
+            transactionLog.pop()
+        }
+    }
+    def buyStock(name, amount, price) {
+        
+        name = name.toLowerCase()
+        if(ingredientStock.get(name) == null) {
+            ingredientStock[name] = 0
+        }
+        ingredientStock[name] += amount
+        
+        transactionLog.last().items << new TransactionItem(
+            name: name,
+            amount: amount,
+            costPerItem: price
+        )
+        
+    }
+   
+    def getAudit() {
+        doAudit(this)
+    }
+    private static doAudit(canteen) {
+        def writer = new StringWriter()
+        def xml = new MarkupBuilder(writer)
+
+        xml.log {   
+            for(trans in canteen.transactionLog) {
+                "transaction" {
+
+                    "type" trans.type
+                    
+                    for(i in trans.items) {
+                        "item" {
+
+                            "name" i.name
+                            "amount" i.amount
+                            "@cost" i.costPerItem
+
+                        }
+                    }
+                    
+                    "timestamp" trans.timestamp
+                    "total cost" trans.totalCost
+                
+                }
+            }
+            
+        }
+        
+        println writer
+        canteen.transactionLog.clear()
+   }
+    
+    def methodMissing(String methodName, args) {
+        System.out.println(methodName)
+    }
+    
+}
+
+Canteen.process {
+    stock {
+        buy "rice", 100 at 1000 each
+        buy "chicken meat", 10 at 10000 total
+    }
+    
+    audit
+}
+
